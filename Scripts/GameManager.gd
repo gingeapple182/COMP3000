83deff31acc3e1250daa7d34d8289690ef8ebcf8
Oliver <129097817+gingeapple182@users.noreply.github.com>
2025-12-16 16:48:58 +0000

    Prototype - Load screen
    New files:
    load_screen.tscn
    - Simple load screen visual
    - Shows to cover scene transitions
    
    Updated:
    GameManager.gd:
    - Reorganised for clarity
    - Removed redundant function
    - Updated to handle load screen

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,16 +1,24 @@
 extends Node
 
-## Scene dictionary
+@onready var load_screen_scene: PackedScene = preload("res://Scenes/Menus/load_screen.tscn")
+var load_screen: CanvasLayer = null
+
+
+func _ready() -> void:
+	load_screen = load_screen_scene.instantiate()
+	load_screen.visible = false
+	add_child(load_screen)
+
+
+## -- scene chanigng functions -- ##
+
 var scenes:= {
 	"landing_menu": preload("res://Scenes/landing_menu.tscn"),
 	"hub_01": preload("res://Scenes/main.tscn"),
 	"puzzle_board": preload("res://Scenes/puzzle_board.tscn"),
 }
-
 var return_scene: PackedScene
 
-
-## functions
 
 func change_scene(scene_id: String) -> void:
 	if not scenes.has(scene_id):
@@ -18,7 +26,16 @@ func change_scene(scene_id: String) -> void:
 		return
 	
 	print("[GameManager] Changing to scene: " + scene_id)
+	
+	if load_screen:
+		load_screen.visible = true
+	
+	await get_tree().create_timer(1.0).timeout
 	get_tree().change_scene_to_packed(scenes[scene_id])
+	await get_tree().process_frame
+	
+	if load_screen:
+		load_screen.visible = false
 
 func set_return_scene_with_id(scene_id: String) -> void:
 	if not scenes.has(scene_id):
@@ -28,10 +45,6 @@ func set_return_scene_with_id(scene_id: String) -> void:
 	return_scene = scenes[scene_id]
 	print("[GameManager] return_scene set by id: " + scene_id)
 	return
-
-func set_return_scene(scene: PackedScene) -> void:
-	return_scene = scene
-	print("[GameManager] return_scene set to: ", return_scene)
 
 func return_to_scene() -> void:
 	if return_scene == null:
